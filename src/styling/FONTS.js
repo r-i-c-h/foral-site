f471e448f2a17d3e3fb4e8624d3e17d383ef50b8
r-i-c-h <rich@richwerden.com>
2019-08-25 23:55:56 -0700

    Typography/Spacing system coming together

--- src/styling/FONTS.js
+++ src/styling/FONTS.js
@@ -1,10 +1,10 @@
 //*** Fonts <link> are imported in SEO.js INSIDE THE <Helmet> SEO Component ***/
+//TODO: FIGURE OUT FONT WEIGHTS/Characters */
 // `font-family: 'Jaldi', sans-serif;`;
 // `font-family: 'Roboto', sans-serif;`;
 // `font-family: 'Roboto Slab', serif;`;
 // `font-family: 'Open Sans', sans-serif;`;
 // `font-family: 'Jacques Francois', serif;`;
-//TODO: FIGURE OUT FONT WEIGHTS/Characters */
 // <link href="https://fonts.googleapis.com/css?family=Jacques+Francois|Jaldi|Open+Sans|Roboto|Roboto+Slab&display=swap" rel="stylesheet">
 // <link href="https://fonts.googleapis.com/css?family=Jacques+Francois|Roboto|Roboto+Slab&display=swap" rel="stylesheet">
 // <link href="https://fonts.googleapis.com/css?family=Jacques+Francois|Open+Sans|Roboto|Jaldi|Roboto+Slab&display=swap" rel="stylesheet">
@@ -14,33 +14,23 @@
 
 const bodyFont = `'Open Sans', sans-serif;`;
 const titleFont = `'Jacques Francois', serif;`;
-
 const googleFontsLink = `<link href="https://fonts.googleapis.com/css?family=Jacques+Francois|Open+Sans&display=swap" rel="stylesheet" />`;
 export { googleFontsLink };
 
 const bodyFontSize = `16px`;
-const bodyFontLineHeight = 1.5;
-const fontModScale = 1.446;
-const FONTS = { bodyFont, titleFont, bodyFontSize, bodyFontLineHeight, fontModScale };
+const bodyLineHeight = 1.5;
+const fontModScale = 1.3;
+
+
+const FONTS = { bodyFont, titleFont, bodyFontSize, bodyLineHeight, fontModScale };
 export default FONTS;
 
-// export const H1 = styled.h1`
-//   font-size: 30px;
-// `;
-// export const H2 = H1.withComponent('h2').extend`
-//   font-size: 24px;
-// `;
-
-// export const H3 = H1.withComponent('h3').extend`
-//   font-size: 18px;
-// `;
-
-// export const H4 = H1.withComponent('h4').extend`
-//   font-size: 15px;
-// `;
-
+// export const H1 = styled.h1` font-size: 30px;`;
+// export const H2 = H1.withComponent('h2').extend` font-size: 24px;`;
+// export const H3 = H1.withComponent('h3').extend` font-size: 18px;`;
+// export const H4 = H1.withComponent('h4').extend` font-size: 15px;`;
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-// System Font Stacks.... FYI
+// System Font Stacks.... Just FYI
 // const monoFonts = `"SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace;`;
 // const gitHubStack = `font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";`;
 // const mediumStack = `font-family: -apple-system, BlinkMacSystemFont,"Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;`;
