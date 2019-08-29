fe1d54de991f005a5dfee014ffdc16fa4b38762a
r-i-c-h <rich@richwerden.com>
2019-08-29 03:09:35 -0700

    Image.js changed to useHook + fix:is visible

--- src/components/image.js
+++ src/components/image.js
@@ -1,32 +1,37 @@
 import React from "react";
-import { StaticQuery, graphql } from "gatsby";
+import styled from "styled-components";
+import { useStaticQuery, graphql } from "gatsby";
 import Img from "gatsby-image";
 
 /*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `StaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
  * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.app/gatsby-image
- * - `StaticQuery`: https://gatsby.app/staticquery
+ * - `gatsby-image`: https://gatsby.dev/gatsby-image
+ * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const Image = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
-            }
+const StyledImg = styled(Img)`
+  width: 100%;
+  /* height: auto;max-width: 300px;border: 10px red solid;display: block;margin: 0 auto; */
+`;
+
+
+const Image = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "article-1-throw_hats.jpg" }) {
+        childImageSharp {
+          fluid{
+            ...GatsbyImageSharpFluid
+            # presentationWidth
           }
         }
       }
-    `}
-    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
-  />
-);
+    }
+  `);
+
+  return (<StyledImg fluid={data.placeholderImage.childImageSharp.fluid} />);
+};
+
 export default Image;
+
+// You can optionally include style info in the Styled img by including  style={{ color: "yellow", opacity: 0.5, width: "50%" }}   etc.
