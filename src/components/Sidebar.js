a33115ed788ac5f08aafbf506bee9be44e89734d
r-i-c-h <rich@richwerden.com>
2019-09-04 17:39:43 -0700

    Tweaks to sidebar layout

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,16 +5,15 @@ import SidebarCard from './SidebarCard';
 
 const TheSidebar = styled.aside`
   z-index: 222;
-  padding: 0.8em;
+  padding: ${props => props.theme.vrt.xxs};
   min-width: 200px;
-  flex: 0 0 20%;
-  background: ${props => props.theme.base};
+  flex: 0 0 25%;
   text-align: center;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  box-shadow: 4px 0px 2px -2px rgba(0,0,0,0.25)
-
+  justify-content: flex-start;
+  background: ${props => props.theme.base};
+  box-shadow: 4px 0px 2px -2px rgba(0,0,0,0.25);
 `;
 
 const Sidebar = () => (
