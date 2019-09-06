79cd80d374dcc6247d6a1ef0b4f0b157642a9ca1
r-i-c-h <rich@richwerden.com>
2019-09-06 13:16:25 -0700

    FIX: LandingPage GatsbyImageQueries Work!

--- src/components/LandingPageArticleLink.js
+++ src/components/LandingPageArticleLink.js
@@ -4,15 +4,17 @@ import styled, { css } from "styled-components";
 import { useStaticQuery, graphql, Link } from "gatsby";
 import Img from "gatsby-image";
 
+// const ImageSecondary = styled.div` flex: 1 0 50%; `;
 const Container = styled(Link)`
-  flex: 1 0 ${props => props.main ? `100%` : `50%`};
+  flex: 1 0 ${props => props.primary ? `100%` : `50%`};
   overflow: hidden;
   position: relative;
   color: ${props => props.theme.whiteish};
   background: ${props => props.theme.blackish};
-  /* border: 1px solid ${props => props.theme.blackish}; */
+  border: 1px solid ${props => props.theme.blackish};
+  height: 40%;
+  max-height: 500px;
 `;
-
 const StyledImage = styled(Img)`
   position: relative;
   top: 0;
@@ -22,84 +24,104 @@ const StyledImage = styled(Img)`
   height: 100%;
   transition: transform 300ms, opacity 300ms;
   &:hover {
-    opacity: 0.8;
+    opacity: 0.7;
     transform: scale(1.1);
   }
 `;
-
-// const ImageSecondary = styled.div` flex: 1 0 50%; `;
 const TextBox = styled.div`
   pointer-events: none;
   position: absolute;
+  margin: 0 auto;
   top: 50%;
-  left: 50%;
-  width: 80%;
-  /* word-break: keep-all; */
-  transform: translate(-50%,-50%);
+  transform: translateY(-50%);
   text-align: center;
-
   padding: ${props => props.theme.vrt.sm};
 `;
 const TextCommonStyles = css`
   color: ${props => props.theme.whiteish};
   z-index: 3;
-  text-shadow: 1px 1px 1px ${props => props.theme.blackish};
+  text-shadow: 1px 1px 1px ${props => props.theme.blackish},
+              0px 0px 10px #000;
 `;
 const LinkTitle = styled.h2`
   ${TextCommonStyles}
-  font-size: ${props => props.main ? props.theme.fsz.h2.remStr : props.theme.fsz.h4.remStr};
-  letter-spacing: ${props => props.main ? '1px' : 'normal'};
+  font-size: ${props => props.primary ? props.theme.fsz.h2.remStr : props.theme.fsz.h4.remStr};
+  letter-spacing: ${props => props.primary ? '1px' : 'normal'};
 `;
-
 const LinkSubtitle = styled.h3`
   ${TextCommonStyles}
+  font-family: ${props => props.theme.bodyFont};
   font-size: ${props => props.theme.fsz.h5.remStr};
 `;
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 // eslint-disable-next-line
-const LandingPageArticleLink = ({ src, alt, title, subtitle, main }) => {
+const LandingPageArticleLink = ({ picSrc, alt, title, subtitle, primary }) => {
   const data = useStaticQuery(graphql`
     query {
-      coverImage: file(
-        relativePath: { regex: "/LandingPage-Main.jpg/" }
-      ) {
-        childImageSharp {
-          fluid(
-            maxWidth: 1200
-            quality: 90
-          ){
+      allImageSharp(filter: {fluid: {originalName: {regex: "/Land/i"}}}) {
+        nodes {
+          fluid {
+            originalName
             ...GatsbyImageSharpFluid
-            # presentationWidth
           }
         }
       }
     }
   `);
+  const targetImage = data.allImageSharp.nodes.find(
+    imgNode => imgNode.fluid.originalName === picSrc
+  );
+  if (!targetImage) {
+    return null;
+  }
   return (
-    <Container to='/' main={main}>
+    <Container to='/' primary={primary}>
       <StyledImage
         alt={alt}
-        fluid={data.coverImage.childImageSharp.fluid}
+        fluid={targetImage.fluid}
         title={title}
       />
-      <TextBox main={main}>
-        <LinkTitle main={main}>{title}</LinkTitle>
+      <TextBox primary={primary}>
+        <LinkTitle primary={primary}>{title}</LinkTitle>
         <LinkSubtitle>{subtitle}</LinkSubtitle>
       </TextBox>
     </Container>);
 };
 
 LandingPageArticleLink.propTypes = {
-  src: PropTypes.string.isRequired,
-  title: PropTypes.string,
+  picSrc: PropTypes.string.isRequired,
   alt: PropTypes.string,
-  subtitle: PropTypes.string
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  primary: PropTypes.bool.isOptional
 };
 export default LandingPageArticleLink;
 
+export const fluidImageFragment = graphql`
+      fragment fluidImageFragment on File {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      `;
 
-// <Image
-//   fluid={image}
-//   style={{ position: `fixed` }}
-//   backgroundColor={imageBackgroundColor ? imageBackgroundColor : false}
-//   title={imageTitle}
-// />
+export const pageQuery = graphql`
+      query {
+        imageOne: file(relativePath: { eq: "one.jpg" }) {
+          ...fluidImage
+        }
+        imageTwo: file(relativePath: { eq: "two.jpg" }) {
+          ...fluidImage
+        }
+      }
+      `;
+
+      // <Image
+      //   fluid={image}
+      //   style={{ position: `fixed` }}
+      //   backgroundColor={imageBackgroundColor ? imageBackgroundColor : false}
+      //   title={imageTitle}
+      // />
--- src/components/LandingPageArticlesGrid.js
+++ src/components/LandingPageArticlesGrid.js
@@ -10,38 +10,37 @@ const LinksContainer = styled.main`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  align-items: center;
-  background: ${props => props.theme.base};
-  max-width: 75%;
+  /* align-items: center; */
+  /* align-items: stretch; */
+  align-content: flex-start;
+  /* background: ${props => props.theme.base}; */
+  /* width: 75%; */
 `;
 
 const LandingPageArticlesGrid = () => (
   <LinksContainer>
     <LandingPageArticleLink
-      main
-      src="LandingPage-Main_topcrop.jpg"
+      primary="true"
+      picSrc="LandingPage-ThrowHats.jpg"
       alt="Graduation hats thrown in the air"
       title="Pathways to Graduation and Beyond"
       subtitle="Practices from two high schools for low-income students"
     />
     <LandingPageArticleLink
-      src="LandingPage-DNA.jpg"
+      picSrc="LandingPage-DNA.jpg"
       alt="A DNA Helix"
       title="Life-course poverty & mid-life health"
       subtitle="Examining the impacts of childhood and adult poverty"
     />
     <LandingPageArticleLink
-      src="LandingPage-Fence.jpg"
+      picSrc="LandingPage-Fence.jpg"
       alt="Fence with razor-wire top"
       title="Mental Health in the House of Corrections"
       subtitle="Evaluating the adequacy of care in NY State Prisons"
     />
   </LinksContainer>
 );
-//   <h1>Hi people</h1>
-//   <p>Welcome to your new Gatsby site.</p>
-//   <p>Now go build something great.</p>
-//   <Image />
+
 //   <Link to="/page-2/">Go to page 2</Link>
 
 export default LandingPageArticlesGrid;
