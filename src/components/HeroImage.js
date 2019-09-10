f2403f32fd24d63d688ee55fbdbf2c949a7494b0
r-i-c-h <rich@richwerden.com>
2019-09-09 20:01:29 -0700

    Adds Dummy Components

---
+++ src/components/HeroImage.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+
+import React from 'react'
+import { Img, useStaticQuery, graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
---
+++ src/components/Pullquote.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+
+import React from 'react'
+import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
